921869fc58cacdd77bb7d5f24845f3d596039875
Nathan Boiron <github@mopolo.net>
2025-11-09 22:17:16 +0100

    Gestion du favicon

--- src/Dto/Antenne.php
+++ src/Dto/Antenne.php
@@ -9,6 +9,7 @@ final readonly class Antenne
     public function __construct(
         public string $code,
         public string $label,
+        public Logo $logo,
         public Links $links,
     ) {}
 }
