0d6f74a78397d79eabca6af4a432c70faa208265
Nathan Boiron <github@mopolo.net>
2025-11-19 12:46:39 +0100

    Update PER-CS to v3

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,7 +7,7 @@ $finder = (new PhpCsFixer\Finder())
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PER-CS2.0' => true,
+        '@PER-CS3x0' => true,
         'no_unused_imports' => true,
     ])
     ->setFinder($finder)
