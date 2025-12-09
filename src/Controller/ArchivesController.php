843f01b2dea3acc89cce3603db2ec3c77bf26933
Nathan Boiron <github@mopolo.net>
2025-12-09 23:09:13 +0100

    Gestion des archives

---
+++ src/Controller/ArchivesController.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\String\s;
+
+#[AsController]
+final readonly class ArchivesController
+{
+    #[Route(
+        path: '/{year}/{month<\d+>}/{day}/{slug}',
+        requirements: [
+            'year' => '[0-9]{4}',
+            'month' => '[0-9]{2}',
+            'day' => '[0-9]{2}',
+            'slug' => '.+',
+        ],
+        host: '{code}.afup.org',
+        priority: 10
+    )]
+    #[Route(
+        path: '/{code}/{year}/{month}/{day}/{slug}',
+        requirements: [
+            'year' => '[0-9]{4}',
+            'month' => '[0-9]{2}',
+            'day' => '[0-9]{2}',
+            'slug' => '.+',
+        ],
+        priority: 10,
+        env: 'dev',
+    )]
+    public function article(string $code, string $year, string $month, string $day, string $slug): RedirectResponse
+    {
+        $slug = s($slug)->ensureEnd('/index.html');
+
+        return new RedirectResponse(
+            sprintf('/archives/%s/%s/%s/%s/%s', $code, $year, $month, $day, $slug),
+            Response::HTTP_MOVED_PERMANENTLY,
+        );
+    }
+
+    #[Route(
+        path: '/{path}.html',
+        host: 'paris.afup.org',
+        priority: 10
+    )]
+    #[Route(
+        path: '/paris/{path}.html',
+        priority: 10,
+        env: 'dev',
+    )]
+    public function paris(string $path): RedirectResponse
+    {
+        // Paris est la seule ville où les articles ne sont pas rangés par date, mais tous à la racine
+        return new RedirectResponse('/archives/paris/' . $path . '.html', Response::HTTP_MOVED_PERMANENTLY);
+    }
+}
